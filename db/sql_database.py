2ff6d00c44c51f59f8d314db49821713fee5eaec
Evellyn Nicole <mevellyn4@gmail.com>
2024-11-24 22:58:16 -0300

    Delete db directory

--- db/sql_database.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.utilities import SQLDatabase
-from config.settings import settings
-
-def get_database():
-    db = SQLDatabase.from_uri(settings.DATABASE_URI, max_string_length=1000000)
-    return db
