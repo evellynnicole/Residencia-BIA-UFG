0de5dd0cf847c05552cf5a72750e4d52a54161dd
Evellyn Nicole <mevellyn4@gmail.com>
2024-11-24 23:19:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -10,40 +10,31 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 
 def main():
-    # Crie as embeddings
     embeddings, sparse_embeddings = create_embeddings()
 
-    # Obtenha o banco de dados vetorial
     collection_name = "test_collection"
     qdrant = get_vectordb(embeddings, sparse_embeddings, url=settings.QDRANT_URL, collection_name=collection_name, docs=None)
 
-    # Configure os retrievers
     retriever_ddl, retriever_sql, retriever_documents = get_retrievers(qdrant)
 
-    # Consulta
     query = "Quem é o artilheiro do campeonato?"
     docs_ddl = retriever_ddl.invoke(query)
     docs_sql = retriever_sql.invoke(query)
     docs_documents = retriever_documents.invoke(query)
 
-    # Obtenha o banco de dados SQL
     db = get_database()
 
-    # Inicialize o LLM
     llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=settings.OPENAI_API_KEY, max_retries=5)
 
-    # Prepare o conteúdo dos documentos
     docs_ddl_content = "\n".join([doc.page_content for doc in docs_ddl])
     docs_sql_content = "\n".join(
         [f"{doc.page_content} (Consulta SQL: {doc.metadata['sql']})" for doc in docs_sql]
     )
     docs_documents_content = "\n".join([doc.page_content for doc in docs_documents])
 
-    # Crie o parser e o prompt para geração de SQL
     parser = JsonOutputParser(pydantic_object=SQLResponse)
     sql_prompt = get_sql_prompt(parser, docs_ddl_content, docs_documents_content, docs_sql_content, query)
 
-    # Crie e execute a cadeia para gerar a SQL
     chain_sql = sql_prompt | llm | parser
     result_sql = chain_sql.invoke({
         "docs_ddl_content": docs_ddl_content,
@@ -52,19 +43,15 @@ def main():
         "query": query
     })
 
-    # Extraia a SQL
     sql = result_sql['sql']
     print(f"SQL Gerada: {sql}")
 
-    # Execute a consulta SQL
     resposta = db.run(sql, include_columns=True)
     print(f"Resposta do Banco de Dados: {resposta}")
 
-    # Prepare o prompt de resposta
     response_prompt = get_response_prompt()
     chain_response = response_prompt | llm | StrOutputParser()
 
-    # Gere a resposta final
     final_result = chain_response.invoke({
         "query": query,
         "sql": sql,
